e136b0b6e29722fd355c166154a2bccee6dc0b6e
bruehlca <carina.bruehl@dataport.de>
2026-08-05 15:40:47 +0200

    reset exporter when unmounted

--- exporter/components/ExporterAddon.vue
+++ exporter/components/ExporterAddon.vue
@@ -34,6 +34,9 @@ export default {
     mounted () {
         this.applyTranslationKey(this.name);
     },
+    unmounted () {
+        this.resetExporterAddon();
+    },
     methods: {
         ...mapActions("Modules/Exporter", [
         ]),
